c4182beb802debb7c1c77e016d7c75f2a5ae8b4a
M0RGaming <chinmaytlc@gmail.com>
2018-09-06 00:09:57 -0600

    Multiple

--- js/functions/tablefilter.js
+++ js/functions/tablefilter.js
@@ -1,13 +1,14 @@
 function filtertable() { 
 
 	var tf = new TableFilter(document.querySelector('#table'), {
-		col_0: "select",
+		col_0: "multiple",
 		col_1: "none",
-		col_2: "select",
+		col_2: "multiple",
 		col_3: "none",
 		col_4: "none",
 		extensions: [{ name: 'sort' }],
 		clear_filter_text: "< Show all >",
+		
 	});
 	tf.init();
 
